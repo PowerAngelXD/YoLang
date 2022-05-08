36ff784b90df92f3e8ca7674439690948c14f839
PowerAngelXD <692732163@qq.com>
2022-05-08 22:02:38 +0800

    [update]优化

--- yolang.cpp
+++ yolang.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include "lexer/lexer.h"
-#include "parser/parser.h"
+#include "vm/yvm.h"
 
 int main(){
     std::string cmd;
@@ -14,7 +13,15 @@ int main(){
                 lex.generate(); // 生成TokenGroup
                 parser::Parser p(lex.getTokenGroup()); // 初始化parser对象，传入tokenGroup
                 auto node = p.parseExpr(); // 开始parse
-                std::cout<<node->toString()<<std::endl;
+                //std::cout<<node->toString()<<std::endl;
+                ygen::ByteCodeGenerator bcg(node);
+                bcg.visitAddExpr(node->addexpr);
+                yvm::YVM vm(bcg);
+                vm.run("null");
+                if(vm.envPeek().first == vm.string || vm.envPeek().first == vm.charcter)
+                    std::cout<<vm.getPool()[vm.envPop().second]<<std::endl;
+                else
+                    std::cout<<vm.envPop().second<<std::endl;
             }
         }
         catch(yoexception::YoError e){
