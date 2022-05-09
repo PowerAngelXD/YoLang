a7553e43007edd794e6cf59d2d8cc18e8f556e1e
PowerAngelXD <692732163@qq.com>
2022-05-09 23:01:14 +0800

    [update]新增加部分设计

--- vm/yvm.h
+++ vm/yvm.h
@@ -2,13 +2,34 @@
 #include <map>
 
 namespace yvm{
+    class YVM;
+
+    /**
+     * @brief Yolang中的Var，Scope，Space实现
+     */
+    namespace var {
+        class Value{
+            int intVal;
+            float deciVal;
+            std::string strVal;
+            std::string charVal;
+            int nullVal = 1; // nullVal默认值为1，当它为0则说明这个Value是一个null
+            std::vector<Value> list; // 列表结构，存放的是Value
+
+            bool isList = false;
+        public:
+
+        };
+    }
+
+
     /**
      * @brief Yolang的VM实例类
      */
     class YVM{
     public:
         // 值在YVM中的类型
-        enum vmVType {iden, iden_text, string, charcter, integer, decimal, null};
+        enum vmVType {iden, iden_text, string, boolean, character, integer, decimal, null}; // 必须与generator中的保持一致
     private:
         // 描述一个YVM中的Value
         typedef std::pair<vmVType, float> vmValue;
