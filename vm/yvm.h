d0f3d0527f77a8e872c4adad4b4e8ffa5b3836ff
PowerAngelXD <692732163@qq.com>
2022-05-14 11:56:16 +0800

    [update]完成对Space，Scope，Value的设计

--- vm/yvm.h
+++ vm/yvm.h
@@ -1,5 +1,6 @@
 #include "../bytecode/generator.h"
 #include <map>
+#include <type_traits>
 
 namespace yvm{
     class YVM;
@@ -8,14 +9,92 @@ namespace yvm{
      * @brief Yolang中的Var，Scope，Space实现
      */
     namespace var {
-        class Value{
-            int intValue;
-            std::vector<Value> list;
-            ygen::paraHelper::typeHelper type;
-
-            bool islist = false;
+        class Space{
         public:
-            bool isList();
+            class Scope{
+            public:
+                class Value{
+                    int intvalue;
+                    float decivalue;
+                    std::string strvalue;
+                    char charvalue;
+                    bool boolvalue;
+                    
+                    std::vector<Value> list;
+
+                    ygen::paraHelper::typeHelper type;
+
+                    bool isconst = false;
+                    bool islist = false;
+                public:
+                    // 构造变量
+                    Value(int val); Value(float val); Value(std::string val); Value(char val); Value(bool val);
+                    // 构造变量或者常量，isc用来检测是不是常量                 
+                    Value(int val, bool isc); Value(float val, bool isc); Value(std::string val, bool isc); Value(char val, bool isc); Value(bool val, bool isc);
+                    // 构造列表
+                    Value(std::vector<Value> list);
+                    // 构造列表，isc用来检测列表是不是常量                
+                    Value(std::vector<Value> list, bool isc);
+
+                    bool isList();   // 判断当前Value是否为列表
+                    bool isConst();  // 判断当前Value是否为Constant
+                    ygen::paraHelper::typeHelper getType(); // 获得Value的type
+
+                    template<class Type>
+                    Type getValue();
+
+                    template<class Type>
+                    void assignValue(Type val);
+                };
+                friend Value;
+                friend Space;
+                typedef std::pair<std::string, Value> storage;
+            private:
+                std::vector<storage> values;
+                std::vector<std::string> memberlist; // 成员名列表
+                std::string idenname; // scope的标识名
+            public:
+                Scope(std::string name);
+
+                bool findValue(std::string name);
+                Value getValue(std::string name);
+                int getPos(std::string name);
+
+                std::string getName();
+            };
+            friend Scope;
+        private:
+            std::vector<Scope> scopestack; // scope栈
+            int deepcount; // 深度计数器
+        public:
+            // 查找所有的Scope中是否有以“name”为名称的value，并返回这个Scope
+            Scope getScope(std::string name);
+            // 查找所有的Scope中是否有以“name”为名称的value，并返回这个Scope对应的索引值
+            int getScopePos(std::string name);
+            // 创建一个新的Scope
+            void createScope(std::string name);
+            // 删除当前Scope，并返回到上一级Scope中
+            void removeScope();
+            // 将当前Scope保存，先删除当前Scope，再返回到上一级Scope中，最后返回删除的Scope
+            Scope PopScope(std::string name);
+
+            // 查找所有的scope（从当前scope一直回溯到globalScope）中是否有以“name”为名称的value
+            bool find(std::string name);
+            // 查找所有的Scope中是否有以“name”为名称的value，并返回这个value
+            Scope::Value getValue(std::string name);
+            // 查找所有的Scope中是否有以“name”为名称的value，并返回这个value对应的索引值
+            int getValuePos(std::string name);
+            // 给名为“name”的value重新赋值
+            template<class Type>
+            void assignValue(std::string name, Type value);
+            // 给名为“name”的value重新赋一个列表
+            template<class Type>
+            void assignValue(std::string name, std::vector<Type> values);
+            // 删除名为“name”的value
+            void deleteValue(std::string name);
+
+            // 获得当前对应的深度值
+            int getDeep();
         };
     }
 
@@ -35,7 +114,7 @@ namespace yvm{
         std::vector<ygen::byteCode> codes;
         std::vector<std::string> constpool;
 
-        // 运行时栈
+        // 运行环境
         std::vector<vmValue> runtimeStack;
         int runtimeTop = 0;
 
