ee3b3474b527377a0cd3c7a558d3e6166e60c4bf
PowerAngelXD <692732163@qq.com>
2022-05-16 23:01:06 +0800

    [fix]优化

--- yolang.cpp
+++ yolang.cpp
@@ -1,29 +1,8 @@
 #include <iostream>
-#include "vm/yvm.h"
+#include "console/yoshell.h"
 
 int main(){
     yvm::YVM vm;
     std::string cmd;
-    std::cout<<"Yolang Shell [Instable-2022-5-0001]"<<std::endl;
-    while(true){
-        try{
-            std::cout<<">>";
-            std::getline(std::cin, cmd);
-            if(cmd == "exit") break;
-            else{
-                yolexer::Lexer lex(cmd); // new lexer object
-                lex.generate(); // make TokenGroup
-                parser::Parser p(lex.getTokenGroup()); // new parser object，enter tokenGroup
-                auto node = p.parse(); // start parse
-                ygen::ByteCodeGenerator bcg(node);
-                bcg.visit(node);
-                vm.reload(bcg.getCodes(), bcg.getConstPool());
-                vm.run("null");
-            }
-            std::cout<<std::endl;
-        }
-        catch(yoexception::YoError e){
-            std::cout<<e.what()<<std::endl;
-        }
-    }
+    ysh::runYoShell();
 }
