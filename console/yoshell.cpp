a21b554ac94a44ea5eb7045912d691a07ff3dcb3
PowerAngelXD <692732163@qq.com>
2022-05-16 23:00:54 +0800

    [new[Yolangshell文件

---
+++ console/yoshell.cpp
@@ -0,0 +1,79 @@
+#include "yoshell.h"
+
+// TOOLS
+std::vector<std::string> ysh::tools::split(std::string str, char sp) {
+    std::vector<std::string> ret;
+    int i = 0;
+    while(i < str.size()) {
+        std::string content;
+        for(; i < str.size(); i ++) {
+            if(str[i] == sp) {i++; break;}
+            content.push_back(str[i]);
+        }
+        ret.push_back(content);
+    }
+    return ret;
+}
+
+// SHELL
+void ysh::insHelp(std::vector<std::string> paras) {
+    std::cout<<"Yoshell helper"<<std::endl;
+    std::cout<<"help       view the help document"<<std::endl;
+    std::cout<<"exit       exit the shell"<<std::endl;
+    std::cout<<"info       view the information of the Yolang"<<std::endl;
+}
+void ysh::insExit(std::vector<std::string> paras) {
+    exit(0);
+}
+void ysh::insInfo(std::vector<std::string> paras) {
+    std::cout<<"Yolang complete version: " << ysh::completeVersion << std::endl;
+    std::cout<<"Yolang version: " << ysh::version << std::endl;
+    std::cout<<"Copyright (c) ITCDT Team."<< std::endl;
+}
+void ysh::insEVAL(std::vector<std::string> paras) {
+    auto eval = paras[0];
+
+    yolexer::Lexer lexer(eval);
+    lexer.generate();
+    auto tg = lexer.getTokenGroup();
+
+    parser::Parser p(tg);
+    auto stmts = p.parse();
+
+    ygen::ByteCodeGenerator bcg(stmts);
+    bcg.visit(stmts);
+
+    yovm.reload(bcg.getCodes(), bcg.getConstPool());
+    yovm.run("null");
+}
+
+void ysh::runYoShell() {
+    // 输出欢迎页面
+    std::cout<<"Welcome! This is YoLang's shell\n" 
+                "We recommend that you enter \"help\" for help\n" 
+                "[current version] "<<ysh::version<<std::endl;
+    std::string in; // 输入内容
+    auto pool = insPool;
+    try{
+        while(true) {
+            std::cout<<">>";
+            std::getline(std::cin, in);
+            // command parser
+            auto tempField = tools::split(in, ' ')[0];
+            bool flag = false;
+            for(int i = 0; i < ysh::insPool.size(); i++){
+                if(tempField == ysh::insPool[i].first) {
+                    auto paras = tools::split(in, ' '); // 创建参数列表
+                    paras.erase(paras.begin()); // 删除第一个元素，因为他是命令解析符
+                    ysh::insPool[i].second(paras);
+                    flag = true;
+                }
+            }
+            if(!flag) insEVAL({in});
+            std::cout<<std::endl;
+        }
+    }
+    catch(yoexception::YoError yoerr){
+
+    }
+}
---
+++ console/yoshell.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "../vm/yvm.h"
+
+namespace ysh{
+    // Tools
+    namespace tools{
+        /**
+         * @brief 分割字符串函数
+         * 
+         * @param str 要分割的字符串
+         * @param sp 分割character
+         * @return std::vector<std::string> 一个vector
+         */
+        std::vector<std::string> split(std::string str, char sp);
+    }
+
+    // 版本号
+    static std::string version = "22.5.4";
+    // 完整版本
+    static std::string completeVersion = "Instable-2022-5-0004";
+    // 指令方法类型
+    typedef void (*insMethod)(std::vector<std::string>);
+    // 指令定义
+    typedef std::pair<std::string, insMethod> ins;
+    // 指令池
+    // 指令方法命名规范：insXXX（XXX为指令名称，首字母大写）
+    void insHelp(std::vector<std::string> paras);
+    void insInfo(std::vector<std::string> paras);
+    void insExit(std::vector<std::string> paras);
+    void insEVAL(std::vector<std::string> paras); // 用于执行输入语句的insMethod
+
+    static std::vector insPool = {ins("help", insHelp), ins("info", insInfo), ins("exit", insExit), ins("", insEVAL)};
+
+    // vm实例
+    static yvm::YVM yovm;
+    
+    void runYoShell();
+}
