7d694585e63572fec3648c673553ad30e53a6f3a
PowerAngelXD <692732163@qq.com>
2022-05-20 22:46:17 +0800

    [fix]更改报错提示

--- exception/yoexc.cpp
+++ exception/yoexc.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 yoexception::YoError::YoError(std::string title, std::string content, int line, int column) {
-    std::string e = "An exception occurred while executing:\n{{" + 
+    std::string e = "An exception is thrown when the program is running:\n{{" + 
                     title + "}}:\n    " + 
                     content + 
                     (line != -1 && column != -1?"\nline: " + std::to_string(line) + ", column: " + std::to_string(column) : " ");
