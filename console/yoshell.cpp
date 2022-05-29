a534c44c21f9fcf4ad82532c8385f73cbf4a43f6
PowerAngelXD <692732163@qq.com>
2022-05-29 22:35:55 +0800

    [update]实现指令view

--- console/yoshell.cpp
+++ console/yoshell.cpp
@@ -18,16 +18,71 @@ bool ysh::tools::compareFileType(std::string name, std::string type) {
     std::size_t index = ((std::string)name).find(type, ((std::string)name).size() - ((std::string)type).size());
     return !(index == std::string::npos);
 }
+std::string ysh::tools::stmtsToString(std::vector<AST::StmtNode*> stmts) {
+    std::string ret = "Stmts: { [";
+    for(auto stmt: stmts){
+        ret += stmt->toString();
+        ret += ", ";
+    }
+    ret += "] }";
+    return ret;
+}
+std::string ysh::tools::mulStr(std::string str, int times) {
+    std::string result;
+    for(int i = 0; i < times; i++) {
+        result += str;
+    }
+    return result;
+}
+std::string ysh::tools::formatAst(std::string aststr) {
+    /* Exaple:
+     * node: {
+     *      node1: {
+     *          node2: {
+     *              token: XXXXX
+     *          }
+     *      }
+     * }
+     * */
+    std::string result;
+    int bigparac = 0; // 大括号出现的计数器
+    for(int i = 0; i < aststr.size(); i ++) {
+        char ch = aststr[i];
+        if(ch == '{') {
+            result += ch;
+            bigparac ++;
+            result += "\n" + mulStr(" ", 4 * bigparac);
+        }
+        else if(ch == '}') {
+            bigparac --;
+            result += "\n" + mulStr(" ", 4 * bigparac);
+            result += ch;
+        }
+        if(ch == '[') {
+            result += ch;
+            bigparac ++;
+            result += "\n" + mulStr(" ", 4 * bigparac);
+        }
+        else if(ch == ']') {
+            bigparac --;
+            result += "\n" + mulStr(" ", 4 * bigparac);
+            result += ch;
+        }
+        else result += ch;
+    }
+    return result;
+}
 
 // SHELL
 void ysh::insHelp(std::vector<std::string> paras) {
     std::cout<<"Yo-Console Helper"<<std::endl;
-    std::cout<<"help....................view the help document"<<std::endl;
-    std::cout<<"exit....................exit the shell"<<std::endl;
-    std::cout<<"run (file)..............run a yolang source file or VM intermediate code file"<<std::endl;
-    std::cout<<"gen (file) (target).....generate a virtual machine file named 'target' from the file named 'file'"<<std::endl;
-    std::cout<<"env (opt).....          operate on virtual machines"<<std::endl;
-    std::cout<<"info....................view the information of the Yolang"<<std::endl;
+    std::cout<<"help........................view the help document"<<std::endl;
+    std::cout<<"exit........................exit the shell"<<std::endl;
+    std::cout<<"run (file)..................run a yolang source file or VM intermediate code file"<<std::endl;
+    std::cout<<"gen (file) (target).........generate a virtual machine file named 'target' from the file named 'file'"<<std::endl;
+    std::cout<<"view (opt) (ins) ...........View the content of \"ins\" in the \"opt\" mode (opt refer to: \"ast\", \"file\")"<<std::endl;
+    std::cout<<"env (opt)...................operate on virtual machines"<<std::endl;
+    std::cout<<"info........................view the information of the Yolang"<<std::endl;
 }
 void ysh::insExit(std::vector<std::string> paras) {
     exit(0);
@@ -69,6 +124,8 @@ void ysh::insRun(std::vector<std::string> paras) {
             yovm.reload(bcg.getCodes(), bcg.getConstPool());
             yovm.run("null");
         }
+        else
+            throw yoexception::YoError("FileError", "Not any file that can be run", -1, -1);
     }
 }
 void ysh::insGen(std::vector<std::string> paras) {
@@ -96,6 +153,39 @@ void ysh::insGen(std::vector<std::string> paras) {
     bcg.genFile(targetName);
     std::cout<<"File generation completed! File name: '"<<targetName<<"'";
 
+}
+void ysh::insView(std::vector<std::string> paras) {
+    std::string key = paras[0];
+    // 处理第一个参数后面的所有参数，全部连在一起，以防出错
+    paras.erase(paras.begin());
+    std::string text;
+    for(auto str: paras)
+        text += str + " ";
+    //
+    if(paras.size() >= 1) {
+        if(key == "ast" || key == "a") {
+            yolexer::Lexer lexer(text);
+            lexer.generate();
+            auto tg = lexer.getTokenGroup();
+            parser::Parser p(tg);
+            auto stmts = p.parse();
+            std::cout<<"View content(AST): "<<std::endl;
+            std::cout<<tools::formatAst(tools::stmtsToString(stmts))<<std::endl;
+        }
+        else if(key == "file" || key == "f") {
+            std::ifstream file(paras[0]);
+            if(!file)
+                throw yoexception::YoError("FileError", "'" + paras[0] + "' is not a valid file name", -1, -1);
+            std::istreambuf_iterator<char> begin(file);
+            std::istreambuf_iterator<char> end;
+            std::string content(begin, end);
+            std::cout<<"View content(FILE): "<<std::endl;
+            std::cout<<content<<std::endl;
+        }
+        else
+            throw yoexception::YoError("ConsoleParaError", "'" + key + "' is not the correct parameter for this instruction", -1, -1);
+    }
+    else throw yoexception::YoError("ConsoleParaError", "Too many or few parameters", -1, -1);
 }
 void ysh::insEVAL(std::vector<std::string> paras) {
     auto eval = paras[0];
@@ -179,7 +269,7 @@ void ysh::runYoShell() {
                 if(tempField == ysh::insPool[i].first) {
                     auto paras = tools::split(in, ' '); // 创建参数列表
                     paras.erase(paras.begin()); // 删除第一个元素，因为他是命令解析符
-                    ysh::insPool[i].second(paras);
+                    ysh::insPool[i].second(paras); // 执行对应的method
                     flag = true;
                 }
             }
