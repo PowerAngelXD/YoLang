3f89557a3215dc66a123513e51357c63df0b4b9c
PowerAngelXD <692732163@qq.com>
2022-06-07 21:41:15 +0800

    [update]优化部分关键字

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -45,7 +45,8 @@ void yolexer::Lexer::generate(){
     std::vector<std::string> yoKeyWords = {
         "var", "ref", "typeof", "nameof", "const", "out", "repeat", 
         "while", "for", "for_each", "return", "func", 
-        "string", "integer", "decimal", "char", "boolean", "delete"
+        "string", "integer", "decimal", "char", "boolean", "delete",
+        "string[]", "integer[]", "decimal[]", "char[]", "boolean[]"
     }; // 新增关键字都在这里
     for(int i = 0; i < input.size(); i++){
         if(input[i] == '\n'){
