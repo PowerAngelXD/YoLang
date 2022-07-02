62fd2ebfb106f6d04be11caaafcf4b37ab571fae
PowerAngelXD <692732163@qq.com>
2022-07-02 10:37:15 +0800

    [version]版本号变更=>Release-2022-7-0060(修正月份错误

--- console/yoshell.h
+++ console/yoshell.h
@@ -32,11 +32,11 @@ namespace ysh{
         std::string stmtsToString(std::vector<AST::StmtNode*> stmts);
     }
     // 版本号
-    static std::string version = "22.6.60";
+    static std::string version = "22.7.60";
     // 发行版本号
     static std::string relVersion = "Yolang 1.1.8";
     // 完整版本
-    static std::string completeVersion = "Release-2022-6-0060";
+    static std::string completeVersion = "Release-2022-7-0060";
     // 指令方法类型
     typedef void (*insMethod)(std::vector<std::string>);
     // 指令定义
