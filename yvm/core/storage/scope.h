76aa8889c9af1cb7f021a623add54646d0954a42
PowerAngelXD <692732163@qq.com>
2022-07-29 22:38:36 +0800

    [update]对行和列两个成员的权限调整

--- yvm/core/storage/scope.h
+++ yvm/core/storage/scope.h
@@ -7,13 +7,13 @@ namespace ysto {
         std::vector<std::string> childNames; // Scope下所有Value的名称列表，与Value实例列表对应
         std::vector<Value> child; // Value实例列表
 
-        int line, column; // 行，列
     public:
+        int line, column; // 行，列
         Scope(std::string iden, int ln, int col);
 
         bool find(std::string name);
         int findPosition(std::string name);
-        Value get(std::string name);
+        Value& get(std::string name);
         void deleteValue(std::string name);
         void createValue(std::string name, Value v);
         std::string getIdentifier();
