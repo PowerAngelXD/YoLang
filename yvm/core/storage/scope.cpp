cf72a12e540d066c19fbbf306233386d30971813
PowerAngelXD <692732163@qq.com>
2022-07-29 22:22:51 +0800

    [update]实现scope的方法

--- yvm/core/storage/scope.cpp
+++ yvm/core/storage/scope.cpp
@@ -1,1 +1,43 @@
 #include "scope.h"
+
+ysto::Scope::Scope(std::string iden, int ln, int col): identifier(iden), column(col), line(ln) {}
+
+bool ysto::Scope::find(std::string name) {
+    for(int i = 0; i < childNames.size(); i ++) {
+        if(name == childNames[i]) return true;
+    }
+    return false;
+}
+
+int ysto::Scope::findPosition(std::string name) {
+    for(int i = 0; i < childNames.size(); i ++) {
+        if(name == childNames[i]) return i;
+    }
+    return -1;
+}
+
+ysto::Value ysto::Scope::get(std::string name) {
+    for(int i = 0; i < childNames.size(); i ++) {
+        if(name == childNames[i]) return child[i];
+    }
+    throw yoexception::YoError("NameError","The value of the identifier named '" + name + "' does not exist in '" + identifier + "'", line, column);
+}
+
+void ysto::Scope::deleteValue(std::string name) {
+    for(int i = 0; i < childNames.size(); i ++) {
+        if(name == childNames[i]) {
+            childNames.erase(childNames.begin() + i);
+            child.erase(child.begin() + i);
+        }
+    }
+    throw yoexception::YoError("NameError","The value of the identifier named '" + name + "' does not exist in '" + identifier + "'", line, column);
+}
+
+void ysto::Scope::createValue(std::string name, ysto::Value v) {
+    childNames.push_back(name);
+    child.push_back(v);
+}
+
+std::string ysto::Scope::getIdentifier() {
+    return identifier;
+}
--- yvm/core/storage/scope.h
+++ yvm/core/storage/scope.h
@@ -6,8 +6,10 @@ namespace ysto {
         std::string identifier; // 标识名
         std::vector<std::string> childNames; // Scope下所有Value的名称列表，与Value实例列表对应
         std::vector<Value> child; // Value实例列表
+
+        int line, column; // 行，列
     public:
-        Scope(std::string iden);
+        Scope(std::string iden, int ln, int col);
 
         bool find(std::string name);
         int findPosition(std::string name);
