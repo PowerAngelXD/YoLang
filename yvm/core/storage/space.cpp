430cac778fdcdb3ee23af0571fc01232460a887b
PowerAngelXD <692732163@qq.com>
2022-07-29 22:39:08 +0800

    [update]Space所有函数实现+优化设计

--- yvm/core/storage/space.cpp
+++ yvm/core/storage/space.cpp
@@ -1,1 +1,57 @@
 #include "space.h"
+
+void ysto::Space::createValue(std::string name, ysto::Value v) {
+    // createValue函数只会在当前Scope中创建新的值
+    scopePool[deepCount].createValue(name, v);
+}
+
+bool ysto::Space::findValue(std::string name) {
+    if(scopePool[deepCount].find(name)) return true;
+    int temp = deepCount; // temp寄存
+    while(temp != -1) {
+        if(scopePool[temp].find(name)) return true;
+        temp --;
+    }
+    return false;
+}
+
+ysto::Value &ysto::Space::getValue(std::string name) {
+    if(scopePool[deepCount].find(name)) return scopePool[deepCount].get(name);
+    int temp = deepCount; // temp寄存
+    while(temp != -1) {
+        if(scopePool[temp].find(name)) return scopePool[temp].get(name);
+        temp --;
+    }
+    throw yoexception::YoError("NameError","The value of the identifier named '" + name + "' does not exist in '" + scopePool[deepCount].getIdentifier() + "'",
+                               scopePool[deepCount].line,
+                               scopePool[deepCount].column);
+}
+
+void ysto::Space::deleteValue(std::string name) {
+    if(scopePool[deepCount].find(name)) scopePool[deepCount].deleteValue(name);
+    int temp = deepCount; // temp寄存
+    while(temp != -1) {
+        if(scopePool[temp].find(name)) scopePool[temp].deleteValue(name);
+        temp --;
+    }
+    throw yoexception::YoError("NameError","The value of the identifier named '" + name + "' does not exist in '" + scopePool[deepCount].getIdentifier() + "'",
+                               scopePool[deepCount].line,
+                               scopePool[deepCount].column);
+}
+
+void ysto::Space::createScope(std::string name, int ln, int col) {
+    scopePool.push_back(Scope(name, ln, col));
+    deepCount ++;
+}
+
+void ysto::Space::deleteScope() {
+    if(deepCount == 0);
+    else {
+        scopePool.erase(scopePool.end());
+        deepCount --;
+    }
+}
+
+ysto::Scope &ysto::Space::getCurrent() {
+    return scopePool[deepCount];
+}
--- yvm/core/storage/space.h
+++ yvm/core/storage/space.h
@@ -6,18 +6,18 @@ namespace ysto {
         std::vector<Scope> scopePool;
         int deepCount = 0;
     public:
-        Space();
+        Space()=default;
         // 创建一个value
         void createValue(std::string name, Value v);
         // 查找指定value
         bool findValue(std::string name);
         // 获取指定value
-        Value getValue(std::string name);
+        Value& getValue(std::string name);
         // 删除指定value
         void deleteValue(std::string name);
 
         // 创建一个scope，深度+1
-        void createScope(std::string name);
+        void createScope(std::string name, int ln, int col);
         // 删除一个scope，深度-1
         void deleteScope();
         // 获取当前scope
