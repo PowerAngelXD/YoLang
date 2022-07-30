ba1743391cc6bdefd7ade9052bd1850643049a5e
PowerAngelXD <692732163@qq.com>
2022-07-30 19:11:24 +0800

    [update]VM设计和部分功能实现

--- yvm/core/yvm.cpp
+++ yvm/core/yvm.cpp
@@ -19,3 +19,101 @@ template<typename Type>
 void vmcore::YStack<Type>::push(Type value) {
     stack.push_back(value);
 }
+
+// VM
+
+vmcore::vm::vm(std::vector<ygen::byteCode> cs, std::vector<std::string> cp) {
+    codes = cs;
+    constPool = cp;
+}
+
+void vmcore::vm::run(std::string arg) {
+    for(auto code: codes) {
+        switch (code.code) {
+            case ygen::add: add(code); break;
+            case ygen::push: push(code); break;
+            case ygen::gto:
+                break;
+            case ygen::jmp:
+                break;
+            case ygen::selfadd:
+                break;
+            case ygen::selfsub:
+                break;
+            case ygen::sub:
+                break;
+            case ygen::div:
+                break;
+            case ygen::mul:
+                break;
+            case ygen::tmo:
+                break;
+            case ygen::idx:
+                break;
+            case ygen::lst:
+                break;
+            case ygen::logicand:
+                break;
+            case ygen::logicor:
+                break;
+            case ygen::no:
+                break;
+            case ygen::lt:
+                break;
+            case ygen::ltet:
+                break;
+            case ygen::gt:
+                break;
+            case ygen::gtet:
+                break;
+            case ygen::equ:
+                break;
+            case ygen::noequ:
+                break;
+            case ygen::gmem:
+                break;
+            case ygen::stf:
+                break;
+            case ygen::listend:
+                break;
+            case ygen::paraend:
+                break;
+            case ygen::scopestart:
+                break;
+            case ygen::scopeend:
+                break;
+            case ygen::idenend:
+                break;
+            case ygen::out:
+                break;
+            case ygen::define:
+                break;
+            case ygen::init:
+                break;
+            case ygen::assign:
+                break;
+            case ygen::del:
+                break;
+            case ygen::call:
+                break;
+        }
+    }
+}
+
+ysto::Value vmcore::vm::getResult() {
+    return valueStack.pop();
+}
+
+void vmcore::vm::add(ygen::byteCode code) {
+    auto right = valueStack.pop();
+    auto left = valueStack.pop();
+    if(left.getType() == ygen::type::vtype::integer) {
+        if(right.getType() == ygen::type::vtype::integer) {
+            valueStack.push(ysto::Value(ytype::YInteger(left.getIntegerValue().get() + right.getIntegerValue().get()), false, -1, -1));
+        }
+    }
+}
+
+void vmcore::vm::push(ygen::byteCode code) {
+    valueStack.push(ysto::Value(ytype::YInteger(code.arg1), false, -1, -1));
+}
--- yvm/core/yvm.h
+++ yvm/core/yvm.h
@@ -12,4 +12,22 @@ namespace vmcore {
         Type peek();
         void push(Type value);
     };
+
+    class vm {
+        std::vector<std::string> constPool; // 存放字符串的池
+        YStack<ysto::Value> valueStack; // 存放value的栈
+        std::vector<ygen::byteCode> codes; // 传入的中间码序列
+        ysto::Space space; // 全局Space
+    public:
+        vm(std::vector<ygen::byteCode> cs, std::vector<std::string> cp);
+        vm()=default;
+
+        // 指令对应的函数实现
+        void add(ygen::byteCode code);
+        void push(ygen::byteCode code);
+        //
+
+        void run(std::string arg);
+        ysto::Value getResult();
+    };
 }
