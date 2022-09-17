b8051263451d35e0df3d21e0df7aceea1db44213
PowerAngelXD <692732163@qq.com>
2022-09-17 17:13:47 +0800

    [update]对新类型系统的适应性更新

--- console/yoshell.cpp
+++ console/yoshell.cpp
@@ -217,19 +217,19 @@ void ysh::insEVAL(std::vector<std::string> paras) {
             std::cout<<"[";
             for(int i = 0; i < result.getList().size(); i ++) {
                 auto elt = result.getList()[i];
-                if(elt.getType() == ytype::vtype::integer)
+                if(elt.getBasicType() == ytype::basicType::integer)
                     std::cout<<elt.getIntegerValue().get();
-                else if(elt.getType() == ytype::vtype::decimal)
+                else if(elt.getBasicType() == ytype::basicType::decimal)
                     std::cout<<elt.getDecimalValue().get();
-                else if(elt.getType() == ytype::vtype::string)
+                else if(elt.getBasicType() == ytype::basicType::string)
                     std::cout<<"\""<<elt.getStringValue().get()<<"\"";
-                else if(elt.getType() == ytype::vtype::boolean) {
+                else if(elt.getBasicType() == ytype::basicType::boolean) {
                     if(elt.getBooleanValue().get())
                         std::cout<<"true";
                     else
                         std::cout<<"false";
                 }
-                else if(elt.getType() == ytype::vtype::null) {
+                else if(elt.getBasicType() == ytype::basicType::null) {
                     std::cout<<"<null>";
                 }
 
@@ -240,19 +240,19 @@ void ysh::insEVAL(std::vector<std::string> paras) {
             std::cout<<"]";
         }
         else{
-            if(result.getType() == ytype::vtype::integer)
+            if(result.getBasicType() == ytype::basicType::integer)
                 std::cout<<result.getIntegerValue().get()<<std::endl;
-            else if(result.getType() == ytype::vtype::decimal)
+            else if(result.getBasicType() == ytype::basicType::decimal)
                 std::cout<<result.getDecimalValue().get()<<std::endl;
-            else if(result.getType() == ytype::vtype::string)
+            else if(result.getBasicType() == ytype::basicType::string)
                 std::cout<<"\""<<result.getStringValue().get()<<"\""<<std::endl;
-            else if(result.getType() == ytype::vtype::boolean) {
+            else if(result.getBasicType() == ytype::basicType::boolean) {
                 if(result.getBooleanValue().get())
                     std::cout<<"true"<<std::endl;
                 else
                     std::cout<<"false"<<std::endl;
             }
-            else if(result.getType() == ytype::vtype::null) {
+            else if(result.getBasicType() == ytype::basicType::null) {
                 std::cout<<"<null>"<<std::endl;
             }
         }
