d42c408d3d8c8248bd3550891a4433cdf32e8273
This is XiaoDeng <104149371+This-is-XiaoDeng@users.noreply.github.com>
2022-10-06 21:05:31 +0800

    [update]规范部分参数写法

--- yolang.cpp
+++ yolang.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[]){
         else{
             if(argc > 1 && argc <= 2) {
                 std::string arg1 = std::string(argv[1]);
-                if(arg1 == "--version" || arg1 == "--v") {
+                if(arg1 == "--version" || arg1 == "-v") {
                     std::cout<<"Yolang version: " << ysh::completeVersion << "(" << ysh::version << ")" << " rel:" << ysh::relVersion << std::endl;
                 }
-                else if(arg1 == "--help" || arg1 == "--h") {
+                else if(arg1 == "--help" || arg1 == "-h") {
                     std::cout<<"Yolang parameter help"<<std::endl;
-                    std::cout<<"--version | --v ................. Get the current version of Yolang"<<std::endl;
-                    std::cout<<"--help | --h .................... Get help with command line arguments"<<std::endl;
-                    std::cout<<"--run <file> ...... ............. Run the yolang file named 'file'"<<std::endl;
+                    std::cout<<"--version | -v ................. Get the current version of Yolang"<<std::endl;
+                    std::cout<<"--help | -h .................... Get help with command line arguments"<<std::endl;
+                    std::cout<<"--run <file> ................... Run the yolang file named 'file'"<<std::endl;
                 }
             }
             else if(argc > 2 && argc <= 3) {
