8f9940b90f9839899fc8f4c5f210bcbde9c68bd0
PowerAngelXD <692732163@qq.com>
2022-10-22 16:09:42 +0800

    [update]front大更新

--- yvm/vmfront.cpp
+++ yvm/vmfront.cpp
@@ -1,1 +1,64 @@
 #include "vmfront.h"
+
+void yvm::start(yvm::yshBase yshObj) {
+    yshObj.yshStart();
+    try {
+        while(true) yshObj.yshUpdate();
+    }
+    catch(yoexception::YoError e){
+        yshObj.catchYoError(e);
+    }
+    catch(yoexception::YshStopFlag flag){}
+    catch(...){}
+    yshObj.yshEnd();
+}
+
+//
+
+yvm::vmfront &yvm::vmfront::newValue(std::string name, ysto::Value value) {
+    vm.space.createValue(name, value);
+    return *this;
+}
+
+yvm::vmfront &yvm::vmfront::clearVM() {
+    vm = vmcore::vm();
+    return *this;
+}
+
+yvm::vmfront &yvm::vmfront::resetVM(std::vector<ygen::byteCode> codes, std::vector<std::string> consts) {
+    vm.load(consts, codes);
+    return *this;
+}
+
+yvm::vmfront &yvm::vmfront::setupVM() {
+    vm.space.createValue("Point", vm.native.bisSet.Point());
+    vm.space.createValue("Application", vm.native.bisSet.Application());
+
+    vm.space.createValue("yolang", vm.native.bisiSet.appFromApplication());
+
+    vm.space.createValue("completeVersion", ysto::Value(ytype::YString(compeleteVersion), true, 0, 0));
+    vm.space.createValue("releaseVersion", ysto::Value(ytype::YString(releaseVersion), true, 0, 0));
+    vm.space.createValue("_integer", ysto::Value(ytype::YString("Integer"), true, 0, 0));
+    vm.space.createValue("_decimal", ysto::Value(ytype::YString("Decimal"), true, 0, 0));
+    vm.space.createValue("_string", ysto::Value(ytype::YString("String"), true, 0, 0));
+    vm.space.createValue("_boolean", ysto::Value(ytype::YString("Boolean"), true, 0, 0));
+
+    for(auto fn: yolexer::bifList) {
+        vm.space.createValue(fn, ysto::Value(ytype::YObject({}, {}, {}, fn), true, 0, 0));
+    }
+
+    return *this;
+}
+
+yvm::vmfront &yvm::vmfront::runVM(std::string arg) {
+    vm.run(-1, arg);
+    return *this;
+}
+
+//
+
+std::vector<yolexer::yoToken> yvm::yshBase::makeTokenGroup(std::string text) {
+    yolexer::Lexer lex(text);
+    lex.generate();
+    return lex.getTokenGroup();
+}
--- yvm/vmfront.h
+++ yvm/vmfront.h
@@ -1,5 +1,46 @@
 #include "core/yvm.h"
 
 namespace yvm{
+    // vm前端，有各个关于vm的操作
+    class vmfront {
+        vmcore::vm vm; // vm实例
+    public:
+        std::string compeleteVersion = "Instable-2022-10-0150";
+        std::string version = "22.10.150";
+        std::string releaseVersion = "Yolang 1.5.29";
 
+        vmfront() = default;
+
+        vmfront& newValue(std::string name, ysto::Value value);
+        vmfront& clearVM(); // clearVM只会把vm中的globalspace内的所有内容清空
+        vmfront& runVM(std::string arg);
+        vmfront& setupVM();
+        vmfront& resetVM(std::vector<ygen::byteCode> codes, std::vector<std::string> consts); // reset只会把vm中的codes和constPool覆写一遍
+    };
+
+    //ysh
+    typedef void (*ins_method)(std::vector<std::string>);
+    typedef std::vector<std::string> ins_method_para;
+
+    class InsSet {
+    public:
+        virtual void __repl__(ins_method_para para)=0; // 规定：每一个ysh程序必须有一个repl
+        virtual void __help__(ins_method_para para)=0; // 规定：每一个ysh程序必须有一个help指令（的函数实现，指令名称可以不是help）
+        virtual void __info__(ins_method_para para)=0; // 规定，每一个ysh程序必须有一个info指令（的函数实现，指令名称可以不是info）
+    };
+
+    class yshBase {
+    public:
+        vmfront front;
+        parser::Parser yparser; // 一个parser
+
+        std::vector<yolexer::yoToken> makeTokenGroup(std::string text);
+        virtual void yshStart(); // 需要继承base类的类进行覆写
+        virtual void yshUpdate();
+        virtual void yshEnd();
+
+        virtual void catchYoError(yoexception::YoError ye);
+    };
+
+    void start(yshBase yshObj);
 }
